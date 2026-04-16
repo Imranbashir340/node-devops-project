b24960d7467f895c74c94ed8d84f1f265c9a8021
Imranbashir340 <Imranbashir340@gcuf.edu.pk>
2026-04-16 11:43:04 +0300

    update to version 4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end("Hello from EC2 via GitHub Actions 🚀 - Version 3!");
+    res.end("Hello from EC2 via GitHub Actions 🚀 - Version 4!");
 });
 
 server.listen(3000, () => {
